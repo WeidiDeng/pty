052587bdbac87b94d9ff58ac61c71683027b0d52
WeidiDeng <weidi_deng@icloud.com>
2026-03-11 11:41:52 +0800

    fix legacy support

--- ioctl_legacy.go
+++ ioctl_legacy.go
@@ -3,8 +3,25 @@
 
 package pty
 
-import "os"
+import (
+	"os"
+	"reflect"
+	"syscall"
+)
 
-func ioctl(f *os.File, cmd uintptr, ptr any) error {
+func ioctl(f *os.File, cmd uintptr, ptr interface{}) error {
 	return ioctlInner(f.Fd(), cmd, ptr) // fall back to blocking io (old behavior)
+}
+
+func ioctlInner(fd, cmd uintptr, ptr interface{}) error {
+	var e syscall.Errno
+	if ptr == nil {
+		_, _, e = syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, uintptr(0))
+	} else {
+		_, _, e = syscall.Syscall(syscall.SYS_IOCTL, fd, cmd, reflect.ValueOf(ptr).UnsafeAddr()) //nolint:gosec // ptr-to-uintptr at syscall site.
+	}
+	if e != 0 {
+		return e
+	}
+	return nil
 }
